84298b9c68d0e5644bb80120786a8e8198e88acc
Jakob Hoeg Mørk <jakmoe01@easv365.dk>
2023-11-02 15:38:25 +0100

    Git

--- ExampleRestAPI/MongoDBRestSettings.cs
+++ ExampleRestAPI/MongoDBRestSettings.cs
@@ -7,5 +7,7 @@
         public string ConnectionString { get; set; } = null;
         public string DatabaseName { get; set; } = null;
         public string StudentsCollectionName { get; set; } = null;
+
+        public string TestENVVar { get; set; } = null;
     }
 }
